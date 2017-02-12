4c8919f35b0a5eced4b389107767be5b967dbf92
Geoffrey Fox <gcf@tempest.dsc.soic.indiana.edu>
2017-02-11 21:10:45 -0500

    adding slurm scripts

--- pathalogy-image-data/scripts/mds/hpcscripts/run.all.sh
+++ pathalogy-image-data/scripts/mds/hpcscripts/run.all.sh
@@ -4,4 +4,4 @@ nodes=28
 name="$nodes"n
 nodefile=nodes.txt
 
-./run.generic.sh 2 12 $name samplerun 2 g $nodes $nodefile 
+sbatch ./run.generic.sh 2 12 $name samplerun 2 g $nodes $nodefile 
--- pathalogy-image-data/scripts/preprocessing/run_single_file_gen.sh
+++ pathalogy-image-data/scripts/preprocessing/run_single_file_gen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cp=/home/pulasthi/git/spidal/applications/pathalogy-image-data/target/pathology_image_dat-jar-with-dependencies.jar
-dataFolder=/home/pulasthi/git/spidal/applications/pathalogy-image-data/sampledata/data
+cp=../../target/pathology_image_dat-jar-with-dependencies.jar
+dataFolder=../../sampledata/data
 FileName=TCGA-55-8094-01Z-00-DX1.8dc29615-e124-4f17-81a1-c0b20c38d12c
-OuputFolder=/home/pulasthi/git/spidal/applications/pathalogy-image-data/sampledata
+OuputFolder=../../sampledata
 OutputFileName=singleimage
 numPoints=2000
 
